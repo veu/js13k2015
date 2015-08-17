12c10689c475ed6545cf6e9778208e68b8807658
Rebecca <becky@monometric.net>
2015-08-17 23:43:40 +0200

    restart game when switching modes

--- src/game.js
+++ src/game.js
@@ -5,7 +5,8 @@ var Game = function (Events, Cube, Ramp, Fighter) {
     var fps = 30;
 
     var map = new Map();
-    var units = [new Fighter(8, 8, 1)];
+    var unitPositions = [];
+    var units = [];
     var placeableElements = [
         new Cube(0, 0, 0),
         new Ramp(0, 0, 0, 'x'),
@@ -17,10 +18,12 @@ var Game = function (Events, Cube, Ramp, Fighter) {
     var tick = 0;
 
     function update() {
-        if (tick % 15 === 0) {
-            units.forEach(function (unit) {
-                unit.move(map);
-            });
+        if (mode === MODE_PLAY) {
+            if (tick % 15 === 0) {
+                units.forEach(function (unit) {
+                    unit.move(map);
+                });
+            }
         }
         tick++;
     }
@@ -36,9 +39,15 @@ var Game = function (Events, Cube, Ramp, Fighter) {
         }
         canvas.translate(canvas.getWidth() / 2, canvas.getHeight() - 180);
         map.render(canvas);
-        units.forEach(function (unit) {
-            unit.render(canvas, map);
-        });
+        if (mode === MODE_EDITOR) {
+            unitPositions.forEach(function (unit) {
+                unit.render(canvas, map);
+            });
+        } else {
+            units.forEach(function (unit) {
+                unit.render(canvas, map);
+            });
+        }
         canvas.pop();
     }
 
@@ -52,6 +61,11 @@ var Game = function (Events, Cube, Ramp, Fighter) {
         var key = String.fromCharCode(event.keyCode);
         if (key === 'E') {
             mode = mode === MODE_PLAY ? MODE_EDITOR : MODE_PLAY;
+        }
+        if (mode === MODE_PLAY) {
+            units = unitPositions.map(function (unit) {
+                return new Fighter(unit.x, unit.y, unit.z);
+            });
         }
     };
 
@@ -71,12 +85,12 @@ var Game = function (Events, Cube, Ramp, Fighter) {
                 target[i] = context.block[i];
             }
             target.z++;
-            units[0].target = target;
+            units.forEach(function (unit) { unit.target = target; });
         }
 
         if (mode === MODE_EDITOR) {
             if (context.type === 'unit') {
-                units = units.filter(function (unit) {
+                unitPositions = unitPositions.filter(function (unit) {
                     return unit.x !== context.unit.x || unit.y !== context.unit.y || unit.z !== context.unit.z;
                 })
                 return;
@@ -89,20 +103,21 @@ var Game = function (Events, Cube, Ramp, Fighter) {
                 var x = block.x + +(context.face === 'x');
                 var y = block.y + +(context.face === 'y');
                 var z = block.z + +(context.face === 'z');
-                if (map.isValid(x, y, z)) {
-                    if (element.type === 'cube') {
-                        map.set(x, y, z, new Cube(x, y, z));
-                    } else if (element.type === 'ramp') {
-                        map.set(x, y, z, new Ramp(x, y, z, element.dir));
-                    } else if (element.type === 'fighter') {
-                        var spotTaken = units.some(function (unit) {
-                            return unit.x === x && unit.y === y && unit.z === z;
-                        });
-                        if (spotTaken) {
-                            return;
-                        }
-                        units.push(new Fighter(x, y, z));
-                    }
+                if (!map.isValid(x, y, z)) {
+                    return;
+                }
+                var spotTaken = unitPositions.some(function (unit) {
+                    return unit.x === x && unit.y === y && unit.z === z;
+                });
+                if (spotTaken) {
+                    return;
+                }
+                if (element.type === 'cube') {
+                    map.set(x, y, z, new Cube(x, y, z));
+                } else if (element.type === 'ramp') {
+                    map.set(x, y, z, new Ramp(x, y, z, element.dir));
+                } else if (element.type === 'fighter') {
+                    unitPositions.push(new Fighter(x, y, z));
                 }
             }
         };
