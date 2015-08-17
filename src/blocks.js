7421e505e152020536aba67c66bb753405014621
Rebecca <becky@monometric.net>
2015-08-17 07:47:32 +0200

    fix cube rendering

--- src/blocks.js
+++ src/blocks.js
@@ -8,7 +8,7 @@ var Cube = function (MapContext) {
         this.type = 'cube';
 
         this.render = function (canvas) {
-            canvas.drawPolygon3d('#ddd', [1,1,0, 1,0,0, 1,0,1, 1,1,1], new MapContext(this, 'x'));
+            canvas.drawPolygon3d('#ddd', [0,0,0, 1,0,0, 1,0,1, 1,1,1, 0,1,1, 0,1,0], new MapContext(this, 'x'));
             canvas.drawPolygon3d('#bbb', [1,1,0, 0,1,0, 0,1,1, 1,1,1], new MapContext(this, 'y'));
             canvas.drawPolygon3d('#fff', [0,0,0, 1,0,0, 1,1,0, 0,1,0], new MapContext(this, 'z'));
         }
