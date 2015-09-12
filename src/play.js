a85bd7cb27eca7490ff7d8f5bec3db95bb913979
Rebecca <becky@monometric.net>
2015-09-12 11:12:29 +0200

    add last level

--- src/play.js
+++ src/play.js
@@ -128,7 +128,7 @@ exports.start = function () {
             }
         },
         {
-            map: "0,4,7,1;2,5,1,5;2,5,2,4;2,5,3,3;2,5,4,2;2,3,1,5;2,3,2,4;2,3,3,3;2,3,4,2.4,0,6.AAQAEABAAAABQAUAHQAAAAAAAAAAEABAAAABQAUAAAAAAAAAAAAAAABAAAABQAUAAAAAAAAAAAAAAAAAAEABQAUAAAAAAAAAAAAAAAAAAAAAQAUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            map: "0,4,7,1;2,5,1,5;2,5,3,3;2,3,1,5;2,3,3,3;1,3,2,4;1,5,2,4;1,3,4,2;1,5,4,2.4,0,6.AAQAEABAAAABQAUAHQAAAAAAAAAAEABAAAABQAUAAAAAAAAAAAAAAABAAAABQAUAAAAAAAAAAAAAAAAAAEABQAUAAAAAAAAAAAAAAAAAAAAAQAUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
             mode: 'weird',
             events: {
                 'start': {
@@ -136,6 +136,30 @@ exports.start = function () {
                 },
                 'won': {
                     msg: ["o_O"]
+                }
+            }
+        },
+        {
+            map: "0,4,7,1;1,0,2,1;1,1,2,1;1,2,0,1;1,2,1,1;2,5,0,1;2,1,4,1;2,0,5,1;2,5,1,1.0,0,6.BAAUAAAAAAAAAAAAAAAAAAAAAAAQAFAAAAAAAAAAAAAAAAAAAAAAAEAAQAEAAAAAAAAAAAAAAAAAAAAAAAEABQAAAAAAAAAAAAAAAAAAAAAABQAUAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            mode: 'weird',
+            events: {
+                'start': {
+                    msg: ["Now these guys are blocking the way? *sigh*"]
+                },
+                'won': {
+                    msg: ["Good thinking!"]
+                }
+            }
+        },
+        {
+            map: "2,1,4,5;2,3,4,5;2,4,1,5;2,4,3,5;1,4,2,3;1,2,4,3;0,8,8,1;2,0,5,2;2,1,5,2;2,2,5,2;2,3,5,2;2,4,5,2;2,5,5,2;2,5,4,2;2,5,3,2;2,5,2,2;2,5,1,2;2,5,0,2;1,7,4,1;1,7,2,1;1,7,0,1;1,4,7,1;1,2,7,1;1,0,7,1;0,8,5,1;1,4,4,8;1,4,2,8;1,4,0,8;1,0,4,8;1,2,4,8.2,2,7.ACUAFABQAkABVSVUFTAzAAAAAAAABAAQAEAAAAFUBQAAAAAAAAAAAAAQAEAAAAAABFAUAAAAAAAAAAAAAEAAAAEABAAQQFUAAAAAAAAAAAAAAAEAAAAQAAAAEQEAAAAAAAAAAABUBVAVQFUAVQFUBQAAAAAAAAAAABARAAAAAQEAABARAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            mode: 'weird',
+            events: {
+                'start': {
+                    msg: ["Looks like this is the one. You can do it!"]
+                },
+                'won': {
+                    msg: ["You‘ve beaten all levels. Awesome!", "Thank you for playing."]
                 }
             }
         }
